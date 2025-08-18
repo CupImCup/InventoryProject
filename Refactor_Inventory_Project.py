dd606cccbc0046982affa850e68cf18c1c8ce357
AntonSchmid98 <alssla_98@web.de>
2025-08-18 19:57:40 +0200

    Added StorageUnit.json a recording of the full inventory as of 17.08.2025, storage units and all, Added proxy request fetcher using free-proxy. Added option to skip daily insert up to specific item. Added Test-folder for quick-and-dirty technology-testing.

--- Refactor_Inventory_Project.py
+++ Refactor_Inventory_Project.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import datetime
+import json
 from pathlib import Path
 from random import random, uniform
 import time
@@ -9,6 +10,7 @@ import pickle
 import psycopg2
 from dotenv import load_dotenv
 import os
+import pythonProxy
 
 
 @dataclass
@@ -18,6 +20,22 @@ class Item:
     users: str
     name: str = ""
     marketable: int = 0
+
+@dataclass
+class jsonItem:
+    Stickers: list
+    Skins: list
+    Cases: list
+    Special_Items: list
+    Major_Sticker_Capsules: list
+    Souvenirs: list
+    Autograph_Capsules: list
+    Passes: list
+    Collectible_Pins: list
+    Patch_Packs: list
+    Sticker_Capsules: list
+    Others: list
+    Patches: list
 
 def takeName(elem):
     return dictionary[elem]
@@ -37,6 +55,7 @@ changesInInventory = os.getenv("changesInInventory") == "True"
 user_id = 1
 dictionary = dict()
 sorted_dictionary = dict()
+#pythonProxy = pythonProxy()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
@@ -71,7 +90,24 @@ QUERY_CURRENT_ITEMS = "SELECT id, name FROM items;"
 cursor.execute(QUERY_CURRENT_ITEMS)
 database_items = cursor.fetchall()
 
+with open("Storage_UNIT_inventory.json", "r", encoding="utf-8") as file:
+    inventory_data = json.load(file)
+database_items = [list(x) for x in database_items]
+
+for category in inventory_data:
+    for item_name in inventory_data[category]:
+        value = inventory_data[category][item_name] 
+        print(item_name, value)
+        
+        # Find the matching sublist in database_items
+        for sublist in database_items:
+            if sublist[1] == item_name:
+                sublist.append(value)  # Add only to the matching item
+
+print(f"Current items in database: {database_items}")
+
 def fetch_inventory():
+    dictionary = {}
     if(changesInInventory):
         ISFAIRREQUEST = False
         for user in users:
@@ -84,7 +120,7 @@ def fetch_inventory():
                     ISFAIRREQUEST = True
                     print("Status code: " + str(inventory.status_code))
                     invData = inventory.json()
-                    dictionary = {}
+
 
                     # Step 1: Count amount and collect user info for each classid
                     for item in invData["rgInventory"].values():
@@ -108,13 +144,14 @@ def fetch_inventory():
                             print(f"Full description: {desc}")
                 else:
                     print("Status code: " + str(inventory.status_code))
-                    print("Will try again in 60 seconds")
-                    time.sleep(60)
+                    #print("Will try again in 60 seconds")
+                    #time.sleep(60)
     else:
-        #Load inventory from file
-        with open(savedDictionaryPath, 'rb') as f:
-            dictionary = pickle.load(f)
-            print("Most recent dictionary loaded from disk")
+        for item in database_items:
+            classid = item[0]
+            name = item[1]
+            amount = item[2] if len(item) > 2 else 1  # Default amount to 1 if not specified
+            dictionary[classid] = Item(classid=classid, name=name, amount=amount, users="thealssla", marketable=1)
 
     #sort inventory
     for item in sorted(dictionary, key=lambda x: dictionary[x].name):
@@ -129,6 +166,7 @@ def fetch_inventory():
 
 def register_items_in_database():
     sorted_dictionary = fetch_inventory()
+    
     # Check if items in sorted_dictionary are already in database
     for item in sorted_dictionary:
         if sorted_dictionary[item].classid not in [i[0] for i in database_items]:
@@ -147,15 +185,19 @@ def register_items_in_database():
 
 #get marketvalue of each item
     #https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=markethashname
-
-
-def fetch_market_and_register_daily_inventory():
+proxies = {
+    "http": "http://127.0.0.1:8080",
+    "https": "http://127.0.0.1:8080",
+}
+def fetch_market_and_register_daily_inventory(skipUntilSpecificItem, itemToSkipTo):
     today =  datetime.datetime.now()
-
-
     today = today.strftime("%Y-%m-%d %H:%M:%S")
     d1 = today.replace("-", "").replace(" ", "").replace(":", "")
+  
+    #old
     sorted_dictionary = fetch_inventory()
+
+    #sorted_dictionary = database_items
     # This will safely construct: PATH/InventoryTables/inventory/05082025.csv
     dataStrPath = PATH / 'InventoryTables' / 'inventory' / f'{d1}.csv'
     # Ensure parent directories exist
@@ -172,19 +214,29 @@ def fetch_market_and_register_daily_inventory():
         if item_obj.marketable != 1:
             print(f"{item_obj.name} is not a marketable item. Skipping this one.")
             continue
-
+        
         ISPRICEFETCHED = False
+        specificItemFound = False
+        specificItem = itemToSkipTo
+        if skipUntilSpecificItem:
+            if not specificItemFound:
+                if item_obj.name == specificItem:
+                    specificItemFound = True
+                    skipUntilSpecificItem = False
+                    print(f"Found specific item: {item_obj.name}")
+                    #continue
+                else:
+                    print(f"Skipping {item_obj.name}")
+                    continue
         while not ISPRICEFETCHED:
-            response = requests.get(
-                f"https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name={item_obj.name}"
-            )
+            response = pythonProxy.fetch_Url(item_obj.name)
             time.sleep(uniform(1.0, 1.8))
-
+            print("Response received for item:", item_obj.name)
+            print(response)
             if response.ok:
                 ISPRICEFETCHED = True
                 market_data = response.json()
-                #print(market_data)
-
+                print(market_data)
                 lowest = market_data.get("lowest_price")
                 median = market_data.get("median_price")
 
@@ -233,8 +285,8 @@ def fetch_market_and_register_daily_inventory():
             else:
                 print(f"There was a problem trying to fetch the price of {item_obj.name}")
                 print(f"Status code: {response.status_code}")
-                print("Too many requests to the marketplace, waiting 60 seconds.")
-                time.sleep(60)
+                #print("Too many requests to the marketplace, waiting 60 seconds.")
+                #time.sleep(60)
 
 
 
@@ -301,14 +353,54 @@ def manual_adding_of_items():
         print(f"Status code: {response.status_code}")
 
 
+
+existing_ids = set()  # replace with your real existing IDs
+
+def generate_class_id(existing_ids):
+    while True:
+        new_id = random.randint(10_000_000, 99_999_999)  # 8-digit number
+        if new_id not in existing_ids:
+            existing_ids.add(new_id)
+            return new_id
+
+def import_inventory_data():
+    print(f"Current items in database: {database_items}")
+    for x in database_items:
+        existing_ids.add(x[0])
+    # Import inventory data from JSON file
+    with open("Storage_UNIT_inventory.json", "r", encoding="utf-8") as file:
+        inventory_data = json.load(file)
+    print(f"Inventory data loaded: {inventory_data['Cases']}")
+    # Process each item in the inventory data
+    for category in inventory_data:
+        for item in inventory_data[category]:
+            print(item, inventory_data[category][item])
+            for x in database_items:
+                item_added = False
+                print(f"Item ID: {x[0]}, Name: {x[1]}")
+                if x[1] == item:
+                    print(f"Item {item} found in database.")
+                elif not item_added:
+                    item_added = True
+                    print(f"Item {item} not found in database, adding it.")
+                    print("Creating new 8-digit classId that does not exists yet.")
+                    new_class_id = generate_class_id(existing_ids)
+                    cursor.execute("INSERT INTO items(id, name, marketable) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;", (new_class_id, item, 1))
+                    existing_ids.add(new_class_id)   
+                    connection.commit()
+
+
+
+
 def main():
     global changesInInventory
     changesInInventory = False  # Set to True to fetch new inventory data
-    
+    skipUntilSpecificItem = False  # Set to True to skip the unit item
+    itemToSkipTo = "Sticker | kRaSnaL | Paris 2023"  # The item to skip to if skipUntilSpecificItem is True
     register_items_in_database()
-    fetch_market_and_register_daily_inventory()
+    fetch_market_and_register_daily_inventory(skipUntilSpecificItem, itemToSkipTo)
     #manual_adding_of_items()
-
+    #import_inventory_data()
     # Close the database connection
     if connection:
         connection.close()
--- steam-microservice/index.js
+++ steam-microservice/index.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const SteamUser = require('steam-user');
-const GlobalOffensive = require('globaloffensive');
+const CS2 = require('globaloffensive');
+const readline = require('readline');
+const ItemNameConverter = require('./convert_Inventory');
+const { writeFile } = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const client = new SteamUser();
-const csgo = new GlobalOffensive(client);
 
+const importInventory = "True" ? true : false;
+const importStorageUnits = "True" ? true : false;
+const importCases = "True" ? true : false;
+const importStickerCapsules = "True" ? true : false;
+const importStickers = "True" ? true : false;
+const importOthers = "True" ? true : false;
+
+
+
+let client = new SteamUser();
+let cs2;
+let nameConverter = new ItemNameConverter();
+nameConverter.initialize();
 let loggedIn = false;
 
 client.logOn({
@@ -17,28 +31,92 @@ client.logOn({
   //twoFactorCode: process.env.STEAM_2FA // optional, if you have 2FA
 });
 
+client.on('steamGuard', (domain, callback) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  // Email code
+  if (domain) {
+    rl.question(`Enter Steam Guard code sent to your email (${domain}): `, (code) => {
+      callback(code.trim()); // continue login with the code
+      rl.close();
+    });
+  } else {
+    // Mobile Authenticator TOTP code
+    rl.question("Enter Steam Guard mobile 2FA code: ", (code) => {
+      callback(code.trim());
+      rl.close();
+    });
+  }
+});
+client.on('error', (err) => {
+  console.error('Error logging into Steam:', err);
+});
 
 client.on('loggedOn', () => {
+  
+  cs2 = new CS2(client);
   loggedIn = true;
   console.log('Steam logged in');
   client.gamesPlayed([730]);
   console.log('Playing CS:GO');
-  csgo.on('connectedToGC', () => {
+  cs2.on('connectedToGC', async () => {
     console.log('Connected to CS:GO Game Coordinator');
-    csgo.inventory.forEach(item => {
-      process.stdout.write(JSON.stringify(item) + '\n');
+    cs2.inventory.forEach(item => {
+      console.log('Name:');
+      process.stdout.write(nameConverter.getItemName(item) + '\n');
+      console.log('Type:');
+      process.stdout.write(nameConverter.getItemType(item) + '\n');
+      console.log('Wear:');
+      process.stdout.write(nameConverter.getWearName(item) + '\n');
+      console.log('Tradable:');
+      process.stdout.write(nameConverter.getItemTradable(item) + '\n');
     });
+
+    let finalItemCounts = {};
+
+    if (importInventory) {
+      const inventoryItemCounts = await processInventory();
+      mergeItemCounts(finalItemCounts, inventoryItemCounts);
+    }
+
+    if (importStorageUnits) {
+      const storageUnitItemCounts = await processStorageUnits();
+      mergeItemCounts(finalItemCounts, storageUnitItemCounts);
+    }
+
+    const outputPath = path.join('/app/output', 'items.json');
+
+  fs.writeFile(outputPath, JSON.stringify(finalItemCounts, null, 2), (err) => {
+    if (err) {
+      process.stderr.write("Error saving config: " + err + '\n');
+    } else {
+      console.log("File saved to:", outputPath);
+    }
   });
-
+    process.stdout.write("Saving config...");
+    writeFile('items.json', JSON.stringify(finalItemCounts, null, 2), (err) => {
+      if (err) {
+        process.stderr.write("Error saving config: " + err + '\n');
+      }
+    });
+    process.stdout.write(JSON.stringify(finalItemCounts, null, 2) + '\n');
+    process.stdout.write("Processing complete.");
+    process.stdout.write("This window will automatically close in 10 seconds.");
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    client.logOff();
+    process.exit(0);
+  });
 });
-
 
 app.get('/inventory', async (req, res) => {
   if (!loggedIn) return res.status(503).send({ error: 'Not logged in yet' });
 
   try {
     const inventory = await new Promise((resolve, reject) => {
-      csgo.requestInventoryContents(730, 2, true, (err, inv) => {
+      cs2.requestInventoryContents(730, 2, true, (err, inv) => {
         console.log('Inventory requested');
         if (err) reject(err);
         else resolve(inv);
@@ -53,3 +131,140 @@ app.get('/inventory', async (req, res) => {
 app.listen(port, () => {
   console.log(`Steam microservice listening on port ${port}`);
 });
+
+
+  async function processInventory() {
+    try {
+      // filter out items that have the casket_id property set from the inventory
+      // because these are items that should be contained in storage units
+      const prefilteredInventory = cs2.inventory.filter((item) => {
+        return !item.casket_id;
+      });
+
+      const convertedItems =
+        nameConverter.convertInventory(prefilteredInventory);
+      const filteredItems = filterItems(convertedItems);
+      const itemCounts = groupAndCountItems(filteredItems);
+      process.stdout.write(`${filteredItems.length} items found in inventory\n`);
+      process.stdout.write(JSON.stringify(itemCounts, null, 2) + '\n');
+      return itemCounts;
+    } catch (err) {
+      process.stderr.write("An error occurred while processing the inventory: " + err + '\n');
+      return {};
+    }
+  }
+
+  async function processStorageUnits() {
+    let finalItemCounts = {};
+    try {
+      const storageUnitIds = getStorageUnitIds();
+      for (const [unitIndex, unitId] of storageUnitIds.entries()) {
+        const items = await getCasketContentsAsync(cs2, unitId);
+        const convertedItems = nameConverter.convertInventory(items);
+        const filteredItems = filterItems(convertedItems);
+        const itemCounts = groupAndCountItems(filteredItems);
+        mergeItemCounts(finalItemCounts, itemCounts);
+        process.stdout.write(
+          `${filteredItems.length} items found in storage unit: ${unitIndex + 1}/${storageUnitIds.length}\n`,
+        );
+        process.stdout.write(JSON.stringify(itemCounts, null, 2) + '\n');
+      }
+      return finalItemCounts;
+    } catch (err) {
+      process.stderr.write("An error occurred while processing storage units: " + err + '\n');
+      return {};
+    }
+  }
+
+  function getStorageUnitIds() {
+    let storageUnitIds = [];
+    for (let item of cs2.inventory) {
+      if (item.casket_contained_item_count > 0) {
+        storageUnitIds.push(item.id);
+      }
+    }
+    return storageUnitIds;
+  }
+
+  function getCasketContentsAsync(cs2, unitId) {
+    return new Promise((resolve, reject) => {
+      cs2.getCasketContents(unitId, (err, items) => {
+        if (err) return reject(err);
+        resolve(items);
+      });
+    });
+  }
+
+  function filterItems(items) {
+    const otherItemTypes = [
+      "Skins",
+      "Special Items",
+      "Agents",
+      "Charms",
+      "Patches",
+      "Patch Packs",
+      "Souvenirs",
+      "Others",
+      "Sticker Capsules",
+      "Collectible Pins",
+      "Collectible Capsules",
+      "Music Kits",
+      "Case Keys",
+      "Passes",
+      "Music Kit Boxes",
+      "Autograph Capsules",
+    ];
+    let filteredItems = [];
+
+    items.forEach((item) => {
+      if (!item.item_tradable) {
+        return;
+      }
+      if (
+        (item.item_type === "Cases" && importCases) ||
+        (item.item_type === "Major Sticker Capsules" &&
+          importStickerCapsules) ||
+        (item.item_type === "Stickers" && importStickers) ||
+        (otherItemTypes.includes(item.item_type) && importOthers)
+      ) {
+        filteredItems.push(item);
+      }
+    });
+    return filteredItems;
+  }
+
+  function groupAndCountItems(items) {
+    let groupedItems = items.reduce((acc, item) => {
+      const { item_name, item_type } = item;
+
+      if (!acc[item_type]) {
+        acc[item_type] = {};
+      }
+
+      if (!acc[item_type][item_name]) {
+        acc[item_type][item_name] = 0;
+      }
+
+      acc[item_type][item_name]++;
+      return acc;
+    }, {});
+
+    return groupedItems;
+  }
+
+  function mergeItemCounts(finalItemCounts, currentItemCounts) {
+    for (const item_type in currentItemCounts) {
+      if (!finalItemCounts[item_type]) {
+        finalItemCounts[item_type] = {};
+      }
+
+      for (const item_name in currentItemCounts[item_type]) {
+        if (!finalItemCounts[item_type][item_name]) {
+          finalItemCounts[item_type][item_name] = 0;
+        }
+
+        finalItemCounts[item_type][item_name] +=
+          currentItemCounts[item_type][item_name];
+      }
+    }
+  }
